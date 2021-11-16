3f08fa5933e8f66b61eb44eda047e0f27ba68d8f
Glitch (rps-game-unit2) <none>
2021-11-16 23:24:14 +0000

    💝🚒 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(".play").click(function() {
+  let input = $(".input").val();
+  $(".userChoice").text(input)
+});
+
+
+$(".play").click(function() {
+  let input = Math.random();
+  if (input < 0.33) 
+  $(".computerChoice").text("rock")
+  if (input > 0.34 < 0.66) 
+  $(".computerChoice").text("paper")
+  if (input > 0.67) 
+  $(".computerChoice").text("scissors")
+});
+
 
